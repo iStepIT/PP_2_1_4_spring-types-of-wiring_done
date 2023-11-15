609606e9c1d9ed242f557f6badc907b9a9caf5cf
iliastepanov <stepanovpro@mail.ru>
2023-11-15 23:03:54 +0300

    task done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-    @Autowired
     private Deth8 deth;
+
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
